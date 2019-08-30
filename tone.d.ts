f903673404770eb4a66b5d9e3d07cb400c6a77a0
Jeff Parrish <jeffs.linux@gmail.com>
2019-08-30 18:04:56 -0400

    adding tsconfig, relevent packages, and beginning basic typings

--- tone.d.ts
+++ tone.d.ts
@@ -1,5 +1,41 @@
 declare module 'tone'{
-	export default Tone;
+	export default ToneNamespace;
+	
+	export type Time = string | number;
+	
+	export type Frequency = string | number;
+
+	export interface ToneNamespace extends Partial<any>{}
+
+	export interface ToneConstructor{
+		new () : Tone;
+		Offline: (callback:Function,duration: Time)
+	}
+
+	export interface Tone {
+		context: Context;
+		receive(channelName: string,channelNumber?: number): this;
+		send(channelName: string, amount: number): GainNode;
+		toFrequency(frequency: Frequency): number;
+	}
+	
+	export interface Context extends Tone {}
+	/** @see INCOMPLETE_DOCUMENTATION */
+	export interface GainNode{} 
+	export interface ToneAudioNode extends AudioNode {
+
+	}
+
+	export interface BufferConstructor{
+		new(url: AudioBuffer | string, onload?: Function, onerror?: Function): Buffer;
+	}
+	export interface Buffer extends Tone {
+		duration: number;
+		length: number ;
+	}
+	export interface ToneContext{} 
+	
+
 }
 
-type Tone = any;
+
